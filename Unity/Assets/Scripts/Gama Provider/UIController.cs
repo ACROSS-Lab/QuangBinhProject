0c00901fd5af192c233e7a72a80f4e8825a9f98a
ptaillandier <patrick.taillandier@gmail.com>
2024-11-08 10:57:29 +0700

    Add new Scene for developping UI

---
+++ Unity/Assets/Scripts/Gama Provider/UIController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour
+{
+
+    public static UIController Instance = null;
+
+    protected bool InVietnamese;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instance = this;
+    }
+
+    public void SetInVietnamese(bool value)
+    {
+        InVietnamese = value;
+    }
+
+    public void StartDikingPhase()
+    {
+
+    }
+
+    public void RestartGame()
+    {
+
+    }
+
+}
