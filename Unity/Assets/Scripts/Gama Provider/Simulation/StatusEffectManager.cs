a3fffea977249f91d7d164ee0b99bdad37993c7d
thedoctor2001 <52832161+thedoctor2001@users.noreply.github.com>
2024-11-08 09:37:27 +0700

    Add the new progress bar

---
+++ Unity/Assets/Scripts/Gama Provider/Simulation/StatusEffectManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Gama_Provider.Simulation
+{
+    public class StatusEffectManager : MonoBehaviour
+    {
+        public GameObject energizedEffect;
+        
+        [SerializeField]
+        private float duration;
+
+        private void Start()
+        {
+            //StartEnergizedEffect(duration);
+        }
+
+        public void StartEnergizedEffect(float customDuration)
+        {
+            energizedEffect.SetActive(true);
+            energizedEffect.transform.Find("RadialProgressBar").GetComponent<CircularProgressBar>()
+                .ActivateCountdown(customDuration);
+
+            StartCoroutine(EndEnergizedEffect(customDuration));
+        }
+
+        IEnumerator EndEnergizedEffect(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            energizedEffect.SetActive(false);
+        }
+    }
+}
