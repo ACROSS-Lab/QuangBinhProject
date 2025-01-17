1dbfc20c39aae6e36ebba55c62f4166470ef3024
thedoctor2001 <52832161+thedoctor2001@users.noreply.github.com>
2025-01-17 16:33:20 +0700

    Update StaticInformation.cs

--- URP Quang Binh v2/Assets/Scripts/Gama Provider/Connection/StaticInformation.cs
+++ URP Quang Binh v2/Assets/Scripts/Gama Provider/Connection/StaticInformation.cs
@@ -1,5 +1,5 @@
 using System.Net;
-
+using System;
 public static class StaticInformation
 {
     public static string endOfGame { get; set; }
@@ -10,10 +10,28 @@ public static class StaticInformation
         if (connectionId == null || connectionId.Length == 0)
         {
             string hostName = Dns.GetHostName(); // Retrive the Name of HOST
-            string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            try
+            {
 
-             string lastIP = myIP.Split(".")[3];
-            connectionId =  "Player_" + lastIP;// + lastIP;
+                IPAddress[] adresses = Dns.GetHostEntry(hostName).AddressList;
+                string myIP = "127.0.0.1";
+                foreach (IPAddress a in adresses)
+                {
+                    if (a.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                    {
+                        myIP = a.ToString();
+                        break;
+                    }
+                }
+                
+                
+                string lastIP = myIP.Contains(".") ? myIP.Split(".")[3] : "0";
+                connectionId = "Player_" + lastIP;// + lastIP;
+            } catch
+            {
+                connectionId = hostName;
+            }
+           
         }
         return connectionId;
     }
